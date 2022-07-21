de36cd8d8fbeec2828c57af6b679fddfa5300249
my-computer-school <gahangwun@gmail.com>
2022-07-21 12:28:48 +0900

    202207211228

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,7 +1,15 @@
-function fetchPage(name){
+function fetchPage1(name){
   fetch(name).then(function(response){
     response.text().then(function(text){
       document.querySelector('#article1').innerHTML = text;
+    })
+  })
+}
+
+function fetchPage2(name){
+  fetch(name).then(function(response){
+    response.text().then(function(text){
+      document.querySelector('#header2').innerHTML = text;
     })
   })
 }
