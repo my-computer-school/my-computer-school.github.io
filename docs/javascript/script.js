d454e45547e242518a7dd9a96d8224c796192cb0
my-computer-school <gahangwun@gmail.com>
2022-11-16 12:23:39 +0900

    202211161223

--- docs/javascript/script.js
+++ docs/javascript/script.js
@@ -41,6 +41,7 @@ function fetchPage3(name3){
   fetch(name3).then(function(response){
     response.text().then(function(text){
       document.querySelector('.header2').innerHTML = text;
+			document.querySelector('.article1').innerHTML = '';
     })
   })
 }
