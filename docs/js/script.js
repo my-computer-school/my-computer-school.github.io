2ee17ea915e619bef4be4fcb4d0d8353c712ab37
my-computer-school <gahangwun@gmail.com>
2022-11-28 17:15:38 +0900

    solid 2022-11-28 17:15

--- docs/js/script.js
+++ docs/js/script.js
@@ -227,6 +227,28 @@ function fetchPageAppChecklist006(appChecklist006){
   })
 }
 
+const dbReq = indexedDB.open('opentutorials',1);
+let db;
+dbReq.addEventListener('success', function(event){
+		db = event.target.result;
+});
+dbReq.addEventListener('error', function(event){
+		const error = event.target.error; 
+		console.log('error', error.name);
+});            
+dbReq.addEventListener('upgradeneeded', function(event){
+		db = event.target.result;  
+		let oldVersion = event.oldVersion;
+		if(oldVersion < 1){
+				db.createObjectStore('topics', {keyPath:'id', autoIncrement:true});
+		} 
+});
+
+
+
+
+
+
 function fetchPage4(name4){
   fetch(name4).then(function(response){
     response.text().then(function(text){
